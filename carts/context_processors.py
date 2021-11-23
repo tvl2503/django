eed0d1945729f905dbc4ad95d3c0c3774afaf004
tvl2503 <79735767+tvl2503@users.noreply.github.com>
2021-11-23 08:55:55 +0700

    Update context_processors.py

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -13,7 +13,7 @@ def counter(request):
             else:
                 cart = Cart.objects.get(cart_id=_cart_id(request=request))
                 cart_items = CartItem.objects.filter(cart=cart)
-            cart_count = sum([cart_item.quantity for cart_item in cart_items])
+            cart_count = len(cart_items)
         except Cart.DoesNotExist:
             cart_count = 0
     return dict(cart_count=cart_count)
